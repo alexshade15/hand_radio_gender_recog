c9ddae9c61ba768fe2f609decb4f229e6c8a75cd
Alex15 <alexshade@live.it>
2020-02-05 17:52:28 +0100

    refactor part2

--- Thresholding.py
+++ Thresholding.py
@@ -3,27 +3,37 @@ import os
 from matplotlib import pyplot as plt
 
 
-def thresholda(name):
-    ''' Thresholds the radiograpys in order to obtain the masks to use in the unet training '''
-    if not (os.path.isfile('./temp/masks' + str(name))):
-        if os.path.isfile("./data/toTrain/" + str(name)):
-            print(name)
-            image = cv2.imread(filename="./data/toTrain/" + str(name))
-            cv2.namedWindow(winname="Grayscale Image", flags=cv2.WINDOW_NORMAL)
-            histogram = cv2.calcHist(images=[image], channels=[0], mask=None, histSize=[256], ranges=[0, 256])
+# import keyboard
 
+def plot_histogram(histogram, name):
+    plt.figure()
+    plt.title(name + " - Grayscale Histogram")
+    plt.xlabel("grayscale value")
+    plt.ylabel("pixels")
+    plt.xlim([0, 256])
+    plt.plot(histogram)
+    plt.show()
+
+
+def thresholda(origin_path, dest_path, name):
+    ''' Thresholds the radiograpys in order to obtain the masks to use in the unet training '''
+    if not (os.path.isfile(dest_path + "masks/" + str(name))):
+        if os.path.isfile(origin_path + str(name)):
+            print(name)
+            image = cv2.imread(filename=origin_path + str(name))
+            # cv2.namedWindow(winname="Grayscale Image", flags=cv2.WINDOW_NORMAL)
+            histogram = cv2.calcHist(images=[image], channels=[0], mask=None, histSize=[256], ranges=[0, 256])
+            plot_histogram(histogram, name)
             ok = 2
-            while ok == 2:
-                plt.figure()
-                plt.title("Grayscale Histogram")
-                plt.xlabel("grayscale value")
-                plt.ylabel("pixels")
-                plt.xlim([0, 256])
-                plt.plot(histogram)
-                plt.show()
+            while ok >= 2:
+                if ok == 3:
+                    plot_histogram(histogram, name)
 
                 k = 11
-                t = int(input('Enter the threshold: '))
+                if ok > 10:
+                    t = ok
+                else:
+                    t = int(input('Enter the threshold: '))
                 blur = cv2.cvtColor(src=image, code=cv2.COLOR_BGR2GRAY)
                 blur = cv2.GaussianBlur(src=blur, ksize=(k, k), sigmaX=0)
                 (t, maskLayer) = cv2.threshold(src=blur, thresh=t, maxval=255, type=cv2.THRESH_BINARY)
@@ -39,21 +49,47 @@ def thresholda(name):
                         cv2.drawContours(myMask2, [c], -1, 0, -1)
                 myMask2 = cv2.bitwise_not(myMask2)
                 myMask = cv2.bitwise_and(myMask2, myMask2, mask=maskLayer)
-                cv2.imshow("After", myMask)
+                while True:
+                    cv2.imshow("Before", image)
+                    cv2.imshow("After", myMask)
+                    if cv2.waitKey(33) == 27:
+                        break
                 cv2.waitKey(delay=10)
-                ok = int(input("ok? y-1, n/r-2: "))
-                if ok == 1:
-                    cv2.imwrite('./temp/masks' + str(name), myMask)
-                    cv2.imwrite('./temp/frames' + str(name), image)
-                    print('./temp/frames' + str(name))
-                    print(os.listdir('./temp/'))
+                ok = int(input("ok? y-1, n/r-2, n/r/h-3: "))
+                if ok == 1 or ok == -1 or ok == -2:
+                    if ok == -2:
+                        cv2.imwrite(dest_path + 'masks/' + "temp_" + str(t) + "_" + str(name), myMask)
+                    else:
+                        cv2.imwrite(dest_path + 'masks/' + str(name), myMask)
+                    cv2.imwrite(dest_path + 'frames/' + str(name), image)
+                    print(dest_path + 'frames/' + str(name))
+                    print(os.listdir(dest_path))
                     print(os.getcwd())
+                    if ok == -1:
+                        return -1
+                    if ok == -2:
+                        return -2
         else:
             print(name, "not found!")
 
 
 if __name__ == "__main__":
-    k = os.listdir("./data/toTrain/")
+    origin_path = "/Users/alex/Desktop/new_normalized_training/"
+    dest_path = "/Users/alex/Desktop/new_groundtruth/"
+    k = [3128, 3131, 3133, 3134, 3136, 3137, 3138, 3140, 3141, 3142, 3143]
+    k += [3144, 3145, 3145, 3148, 3149, 3151, 3153, 3156, 3157, 3164, 3165]
+    k += [3167, 3169, 3172, 3175, 3176, 3179, 3181, 3182, 3183, 3185, 3186]
+    k += [3187, 3188, 3189, 3196, 3200, 3201, 3202, 3204, 3205, 3211, 3213]
 
+    for index, elem in enumerate(k):
+        k[index] = str(elem) + ".png"
+
+    print(len(k))
     for elem in k:
-        thresholda(elem)
+        mode = thresholda(origin_path, dest_path, elem)
+        if mode == -1:
+            break
+        if mode == -2:
+            mode = thresholda(origin_path, dest_path, elem)
+        if mode == -1:
+            break
--- oldCode/cropping.py
+++ oldCode/cropping.py
@@ -1,7 +1,6 @@
 from PIL import Image
 import glob
 
-
 def make_square(im, min_size=256, fill_color=(0, 0, 0, 0)):
     x, y = im.size
     size = max(min_size, x, y)
@@ -10,13 +9,35 @@ def make_square(im, min_size=256, fill_color=(0, 0, 0, 0)):
     return new_im
 
 
-photos = glob.glob("/data/validation/boneage-validation-dataset-2/*.png")
+# photos = glob.glob("/Users/alex/Desktop/prova/images/mass/*.tif")
+# photos = ["/Users/alex/Desktop/20586908_6c613a14b80a8591_MG_R_CC_ANON.png"]
+photos = ["/Users/alex/Desktop/new_training/3752.jpg"]
+# photos = glob.glob("/Users/alex/Desktop/new_training/*.jpg")
 for photo in photos:
     name = photo.split("/")[-1]
-    print(name)
+    tmp = name.split(".")
+    print(tmp[0], tmp[1])
     img = Image.open(photo)
     img = make_square(img)
+    print("Squared")
     width = 512
     height = 512
     img = img.resize((width, height), Image.ANTIALIAS)  # best down-sizing filter
-    img.save("/data/normalized/validation2/" + name, "PNG")
+    print("Resized")
+    img.save("/Users/alex/Desktop/new_normalized_training/" + tmp[0] + ".png", "PNG")
+    print("Saved\n\n")
+
+# import cv2
+# import os
+#
+# print(os.getcwd())
+# filename = '/Users/alex/Desktop/prova/images/mass/20586908_6c613a14b80a8591_MG_R_CC_ANON.tif'
+# W = 1000.
+# img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+# imgScale = W / img.shape[1]
+# newX, newY = img.shape[1] * imgScale, img.shape[0] * imgScale
+# newimg = cv2.resize(img, (int(newX), int(newY)))
+#
+# cv2.imwrite("resizeimg.jpg", newimg)
+# cv2.imshow("Show by CV2", newimg)
+# cv2.waitKey(0)
